426d3ca6e1be24cdd40036a6c8c406393b1a979a
mariod8 <mariodavo.20@gmail.com>
2022-05-23 10:31:58 +0200

    Files

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,2 +1,2 @@
 # Format all cpp-ish files in curr dir and sub dirs
-find . -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(cpp\|hpp\|cc\|cxx\|h\|cu\|c\)' -exec clang-format -style=file -i {} \;
